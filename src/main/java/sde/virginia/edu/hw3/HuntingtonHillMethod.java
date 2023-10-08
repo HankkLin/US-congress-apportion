04883b0ea1205f7e3625273795098e30694f93e8
Hank <ezp9xp@virginia.edu>
2023-10-08 17:03:53 -0400

    add HuntingtonHillMethod.java

---
+++ src/main/java/sde/virginia/edu/hw3/HuntingtonHillMethod.java
@@ -0,0 +1,9 @@
+package sde.virginia.edu.hw3;
+import java.util.List;
+
+public class HuntingtonHillMethod implements ApportionmentMethod {
+    @Override
+    public Representation getRepresentation(List<State> states, int targetRepresentatives) {
+        return null;
+    }
+}
