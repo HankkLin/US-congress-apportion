59de5bf66c040c3b851a2b1bddbb2c1207cf320a
Hank <ezp9xp@virginia.edu>
2023-10-10 19:50:20 -0400

    Forgot to implement order in RepresentationFormatFactory.java

--- src/main/java/sde/virginia/edu/hw3/RepresentationFormatFactory.java
+++ src/main/java/sde/virginia/edu/hw3/RepresentationFormatFactory.java
@@ -4,6 +4,18 @@ public class RepresentationFormatFactory {
     public RepresentationFormat getDefaultFormat(){
         return this.getFormat("alphabet");
     }
+    public RepresentationFormat getFormat(String name, DisplayOrder order){
+        switch (name){
+            case ("alphabet"):
+                return new AlphabeticalFormat();
+            case ("benefit"):
+                return new BenefitFormat(order);
+            case ("population"):
+                return new PopulationFormat(order);
+            default:
+                throw new IllegalArgumentException(name + " is not a kind of format. Please input alphabet, benefit, or population.");
+        }
+    }
     public RepresentationFormat getFormat(String name){
         switch (name){
             case ("alphabet"):
